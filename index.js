e683a6093ef6fc3b073f7d0c4a2597ee0f2297b8
2atenea1 <dayannarondon0426@gmail.com>
2022-11-25 20:24:45 -0500

    se hizo hasta conocimientos

---
+++ index.js
@@ -0,0 +1,14 @@
+const usuario ="astrid";
+const contra =2126;
+
+function iniciarSesison(){
+  if(usuario=="astrid" && contra=="2126"){
+     alert("bienvenido");
+     window.location="/pages/inicio.html";
+    }else{
+       console.log("usuario y contraseña son incorrectos, intenta de nuevo.")
+     }
+   
+    }
+
+    
